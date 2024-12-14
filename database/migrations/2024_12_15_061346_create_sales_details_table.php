b26d5c6bd9fa76d6602c76d3dfdbbaf66f6f5ce7
vian151004 <vianmaulana540@gmail.com>
2024-12-15 06:17:51 +0700

    re-migrate sales & sales_details

---
+++ database/migrations/2024_12_15_061346_create_sales_details_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('sales_details', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('sales_id');
+            $table->unsignedBigInteger('product_id');
+            $table->integer('sell_price');
+            $table->integer('subtotal');
+            $table->tinyInteger('discount')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('sales_details');
+    }
+};
